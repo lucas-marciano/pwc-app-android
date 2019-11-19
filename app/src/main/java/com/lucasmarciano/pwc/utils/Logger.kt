1251515abff3bc9c8d83e159a4f979ed923dcf4c
Lucas Marciano <lucasmarciano@inatel.br>
2019-11-19 09:01:47 -0300

    (custom): inserted the documentation, the logger file and make the adjusts in build.gradle.
    Signed-off-by: Lucas Marciano <lucasmarciano@inatel.br>

---
+++ app/src/main/java/com/lucasmarciano/pwc/utils/Logger.kt
@@ -0,0 +1,10 @@
+/*
+* Developed by: Inatel Competence Center
+* Copyright 2019, Inalel
+* All rights are reserved. Reproduction in whole or part is
+* prohibited without the written consent of the copyright owner.
+*/
+package com.lucasmarciano.pwc.utils
+
+class Logger {
+}
